5f783e1511df17b61b320e2b6193cf1bac460b70
Jefte Santos <jeftenunes@hotmail.com>
2020-04-30 20:03:51 -0400

    [master] resolution of the section clippy

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    clippy1();
+    clippy2();
+}
+
+fn clippy1() {
+    let x = 1.2331f64;
+    let y = 1.2332f64;
+    let error = 0f64; 
+    if (y - x).abs() > error  {
+        println!("Success!");
+    }
+}
+
+fn clippy2() {
+    let mut res = 42;
+    let option = Some(12);
+    if let Some(x) = option {
+        res += x;
+    }
+
+    println!("{}", res);
+}
